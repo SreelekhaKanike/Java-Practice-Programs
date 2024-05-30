3fcb2457be754817568507b30ccc12794929a4f5
SreelekhaKanike <sreelekhakanike@gmail.com>
2024-05-30 16:28:56 +0530

    Add files via upload

---
+++ Art.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+public class Art{
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		a=sc.nextInt();
+		b=sc.nextInt();
+	c=a+b;
+		System.out.println("sum is:"+c);
+		c=a-b;
+		System.out.println("sub is:"+c);
+		c=a*b;
+		System.out.println("mul is:"+c);
+		c=a/b;
+		System.out.println("div is:"+c);
+		c=a%b;
+		System.out.println("mod is:"+c);
+		
+		
+		
+
+	}
+
+}
---
+++ ArtheAssignOpe.java
@@ -0,0 +1,17 @@
+public class ArtheAssignOpe {
+	
+public static void main(String[] args) {
+int a=15,b=5;
+a+=b;
+System.out.println(a);
+a-=b;
+System.out.println(a);
+a*=b;
+System.out.println(a);
+a/=b;
+System.out.println(a);
+a%=b;
+System.out.println(a);
+
+}
+}
